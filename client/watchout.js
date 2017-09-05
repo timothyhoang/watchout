ff6923eacc8a91ac8c6c51a4ddbd7a7677f57252
Hack Reactor Student <student@hackreactor.com>
2017-09-05 15:00:30 -0700

    Added draggable circle for player

--- client/watchout.js
+++ client/watchout.js
@@ -7,8 +7,8 @@ var init = function(n, r) {
   width = board.attr('width');
   nCircles = n;
   circleRadius = r;
-  generateNewPositions();
-  enter();
+  initializeMouse();
+  initializeEnemies();
 };
 
 var generateNewPositions = function() {
@@ -20,29 +20,51 @@ var generateNewPositions = function() {
   }
   
   circlePositions.forEach(function(circlePosition) {
-    circlePosition.x = Math.min(width - circleRadius, (Math.random() * width) + circleRadius);
-    circlePosition.y = Math.min(height - circleRadius, (Math.random() * height) + circleRadius);
+    circlePosition.x = Math.random() * (width - 2 * circleRadius) + circleRadius;
+    circlePosition.y = Math.random() * (height - 2 * circleRadius) + circleRadius;
   });
 };
 
-var enter = function() {
-  board.selectAll('circle')
+var initializeEnemies = function() {
+  generateNewPositions();
+  
+  board.selectAll('circle.enemy')
        .data(circlePositions)
        .enter().append('circle')
+         .attr('class', 'enemy')
          .attr('cx', function(d) { return d.x; })
          .attr('cy', function(d) { return d.y; })
-         .attr('r', circleRadius);  
+         .attr('r', circleRadius);
 };
 
 var update = function() {
   generateNewPositions();
-  board.selectAll('circle')
+  
+  board.selectAll('circle.enemy')
        .data(circlePositions)
-       .transition(1000)
+       .transition().duration(2500)
        .attr('cx', function(d) { return d.x; })
        .attr('cy', function(d) { return d.y; })
        .attr('r', circleRadius);  
 };
 
+var initializeMouse = function() {
+  board.selectAll('circle.mouse')
+       .data([{x: .5 * width, y: .5 * height}])
+         .attr('fill', 'orange')
+         .attr('cx', function(d) { return d.x; })
+         .attr('cy', function(d) { return d.y; })
+         .attr('r', circleRadius)
+       .call(d3.behavior.drag().on('drag', handleMouseDrag));
+};
+
+var handleMouseDrag = function(d) {
+  d3.select(this)
+    .attr('cx', d.x = d3.event.x)
+    .attr('cy', d.y = d3.event.y);
+};
+
 init(10, 10);
-setInterval(update, 1000);
+setInterval(update, 2500);
+
+d3.selectAll('circle.mouse').on('.drag', function() { handleMouseDrag(); });
